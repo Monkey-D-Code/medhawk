e14671ed8230571484c0aac5e6e289f4a0cbd28c
Monkey-D-Code <theartofdeception08@gmail.com>
2019-10-26 15:03:29 +0530

    db fix

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('medhawk','root','',{
+const sequelize = new Sequelize('medhawk','medhawki','UttamMah@ta1',{
     dialect : 'mysql',
     host : 'localhost',
 });
